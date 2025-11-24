8455c76b7b0eef95afc4a01a4460723bbc735735
rahulvishwakarma0111 <ashishuniyalaionos>
2025-11-24 16:23:54 +0530

    hover effect added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { use } from 'react'
 import './Navbar.css'
-import { useNavigate } from 'react-router-dom';
+import { href, useNavigate } from 'react-router-dom';
 
 const link = 'https://business.true.th/en/home?_gl=1*kvblng*_gcl_au*MTA5MzYxOTAzMS4xNzYzNTMzMDA4*_ga*MTU3NjE0MDA0My4xNzYzNTMzMDA5*_ga_KB786LL39X*czE3NjM5Nzk2MjQkbzExJGcxJHQxNzYzOTgwNDE3JGo1MCRsMCRoMA..'
 
@@ -15,13 +15,18 @@ const Navbar = () => {
       <div className="topbar">
         <div className="topbar-inner">
           <nav className="top-links">
-            <a href="https://www.true.th/our-company" style={{fontSize: '16px'}} target='_blank'>Our Company</a>
-            <a href={link} style={{fontSize: '16px'}} target='_blank'>Corporate Customer</a>
-            <a href={link2} style={{fontSize: '16px'}} target='_blank'>Find True Shop</a>
+            <a href="https://www.true.th/our-company" style={{ fontSize: '16px' }} target='_blank'>Our Company</a>
+            <a href={link} style={{ fontSize: '16px' }} target='_blank'>Corporate Customer</a>
+            <a href={link2} style={{ fontSize: '16px' }} target='_blank'>Find True Shop</a>
           </nav>
           <div className="top-right">
             <img src='https://images.contentstack.io/v3/assets/blt8ba403bee4433fd8/blt4a88971de999d1e3/App_Icon.svg' height={'28px'} width={'28px'}></img>
-            <div className="app-label">True App</div>
+            <div
+              className="app-label"
+              onClick={() => {
+                window.location.href = "https://www.true.th/services/true-app";
+              }}
+            >True App</div>
           </div>
         </div>
       </div>
